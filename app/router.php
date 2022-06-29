edae7a5b535bdcd24a5bae2ad0e34c6a09d1a542
Nazik4114 <nazikban14@gmail.com>
2022-06-29 22:22:18 +0300

    fixed oll errors

--- app/router.php
+++ app/router.php
@@ -9,7 +9,6 @@ try {
     list($controller, $method) = $handler;
     $controller->$method();
 } catch(Exception $error) {
-    // реакція на unknown route  знаходиться на рівень вище
     die('Sorry, internal application error happened: ' . $error->getMessage());
 }
 
